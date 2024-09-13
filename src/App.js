50133755ef6b4b0df215bf85e4556a089bd4b316
amankashyap510 <amankashyap510@gmail.com>
2024-09-13 13:29:39 +0530

    Basic UI set-up and styling completed.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
 import './App.css';
+import Home from './screens/Home';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+import Login from './screens/Login';
+import '../node_modules/bootstrap-dark-5/dist/css/bootstrap-dark.min.css';
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle';
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js';
 
 function App() {
   return (
-  <div>Hello Daad</div>
+    <Router>
+     <div>
+      <Routes>
+        <Route exact path="/" element={<Home />}></Route>
+        <Route exact path="/login" element={<Login />}></Route>
+      </Routes>
+     </div>
+    </Router>
+ 
   );
 }
 
